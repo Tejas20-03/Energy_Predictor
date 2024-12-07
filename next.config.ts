ee1d220deb1b4a971a7a0260ba1803cdde4139c6
tejaschhabra.official <tejaschhabra.official@gmail.com>
2024-12-07 05:21:55 +0000

    Layout update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['var(--font-poppins)'],
+      },
+    },
+  },
+  plugins: [],
 };
 
 export default nextConfig;
